7d6775ad1d47e36f4900764755346a2c15a0fe8e
Wes Bethel <ewbethel@lbl.gov>
2024-09-17 17:32:58 -0700

    changing from CblasColMajor to CblasRowMajor for blas-related computations

--- dgemm-blas.cpp
+++ dgemm-blas.cpp
@@ -11,5 +11,5 @@ const char* dgemm_desc = "Reference dgemm.";
  * via the standard FORTRAN interface - hence the reference semantics.
  */
 void square_dgemm(int n, double* A, double* B, double* C) {
-    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1., A, n, B, n, 1., C, n);
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1., A, n, B, n, 1., C, n);
 }
